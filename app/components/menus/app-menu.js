469c37392e8dc58690c6b4eda965461952f57c5c
sandoval-home <john.x.sandoval@gmail.com>
2016-03-22 21:20:48 -0600

    Generate LESS core file structure - Also created an empty nav menu component to illustrate a sample   folder structure

---
+++ app/components/menus/app-menu.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    // Ember hook for setting the component container element
+    tagName: 'div',
+
+    // Ember hook for setting CSS classes on the component container element
+    classNames: ['app-menu']
+
+});
